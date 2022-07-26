a369e06298c8d826bc62a4117a28e6251013ab42
Timothy Warrington <tim.warrin@gmail.com>
2022-07-26 13:16:25 -0600

    add trpc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-import type { NextPage } from "next";
+import { trpc } from "../utils/trpc";
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+export default function IndexPage() {
+  const hello = trpc.useQuery(["hello", { text: "client" }]);
+  if (!hello.data) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <p>{hello.data.greeting}</p>
+    </div>
+  );
+}
 
-export default Home;
+// import type { NextPage } from "next";
+
+// const Home: NextPage = () => {
+//   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+// };
+
+// export default Home;
