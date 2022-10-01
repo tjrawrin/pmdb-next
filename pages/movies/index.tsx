2230cb6cbd12c1c0594bc4b3fd9b2c9834234d68
Rick Warrington <phial1020@gmail.com>
2022-10-01 15:01:42 -0600

    Fix react error

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -60,7 +60,7 @@ const Movies: NextPage = () => {
       />
       <h2 className="text-2xl mb-6 border-b-2">
         {data?.option.toUpperCase()}
-        <span class="text-sm">{`Showing ${data?.results.length} results.`}</span>
+        <span className="text-sm">{`Showing ${data?.results.length} results.`}</span>
       </h2>
       {filteredMovies && filteredMovies.length > 0 ? (
         <ul>
