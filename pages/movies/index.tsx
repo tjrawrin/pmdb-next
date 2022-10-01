52a4d35f895d00624b50c7d960c75700b34f9762
Rick Warrington <phial1020@gmail.com>
2022-10-01 15:34:25 -0600

    Fix layout on result count text

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -60,7 +60,7 @@ const Movies: NextPage = () => {
       />
       <h2 className="text-2xl mb-6 border-b-2">
         {data?.option.toUpperCase()}
-        <span className="text-sm">{`Showing ${data?.results.length} results.`}</span>
+        <span className="text-sm" style={{paddingLeft: '16px'}}>{`Showing ${data?.results.length} results.`}</span>
       </h2>
       {filteredMovies && filteredMovies.length > 0 ? (
         <ul>
